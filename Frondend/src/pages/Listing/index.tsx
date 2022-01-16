de4e8dfcf1f8ef615b303eef254897eb41ab72f8
edenio <edenio_cdi@yahoo.com.br>
2022-01-16 16:51:04 -0300

    MovieCard

--- Frondend/src/pages/Listing/index.tsx
+++ Frondend/src/pages/Listing/index.tsx
@@ -1,10 +1,43 @@
 import Pagination from "components/pagination";
+import MovieCard from "components/MovieCard";
 
-function Listing (){
+function Listing() {
 
-    return(
-    
-        <Pagination/>
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+
+                    </div>
+                   
+
+                </div>
+
+            </div>
+
+        </>
+
     );
 }
 
