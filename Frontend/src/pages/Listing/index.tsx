248d4ce5f6b3961dacaabb804e4e98c59f275a47
edenio <edenio_cdi@yahoo.com.br>
2022-05-15 20:56:20 -0300

    Correções

--- Frontend/src/pages/Listing/index.tsx
+++ Frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
 
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/Movies?size=12&page=${pageNumber}`)
             .then(response => { 
                 const data = response.data as MoviePage;
                 setPage(data);
